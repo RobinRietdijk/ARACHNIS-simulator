7785148a7d66435de5bf4e0c2c14d7b5534224b3
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2023-10-26 01:35:19 +0200

    Add radians

--- src/components/Geometry/Polygon.js
+++ src/components/Geometry/Polygon.js
@@ -1,4 +1,4 @@
-import { toDegrees, toRadians } from "./util";
+import { toRadians } from "./util";
 
 class Polygon {
     constructor(vertices, edge_length, height = 100) {
@@ -11,37 +11,16 @@ class Polygon {
         const a_angle = toRadians(360 / vertices);
         const b_angle = toRadians((180 - (360 / vertices)) / 2);
         const radius = edge_length * Math.sin(b_angle) / Math.sin(a_angle);
-        console.log(radius)
 
         let angle = 0;
-        while (angle <= 360) {
-            let x_coordinate, y_coordinate;
-            if (angle == 0) {
-                x_coordinate = 0;
-                y_coordinate = radius;
-            }
-            else if (0 < angle && angle <= 90) {
-                x_coordinate = Math.cos(toRadians(90 - angle)) * radius;
-                y_coordinate = Math.sin(toRadians(90 - angle)) * radius;
-            }
-            else if (90 < angle && angle <= 180) {
-                x_coordinate = Math.cos(toRadians(angle - 90)) * radius;
-                y_coordinate = Math.sin(toRadians(angle - 90)) * radius * -1;
-            }
-            else if (180 < angle && angle <= 270) {
-                x_coordinate = Math.cos(toRadians(270 - angle)) * radius * -1;
-                y_coordinate = Math.sin(toRadians(270 - angle)) * radius * -1;
-            }
-            else if (270 < angle && angle <= 360) {
-                x_coordinate = Math.cos(toRadians(angle - 270)) * radius * -1;
-                y_coordinate = Math.sin(toRadians(angle - 270))* radius;
-            }
-            else throw "Exception";
+        while (angle <= 2 * Math.PI) {
+            const x_coordinate = Math.cos(angle) * radius;
+            const y_coordinate = Math.sin(angle) * radius;
 
             this.coordinates.x.push(x_coordinate);
             this.coordinates.y.push(y_coordinate);
             this.coordinates.z.push(height);
-            angle += toDegrees(a_angle);
+            angle += a_angle;
         }
     }
 
