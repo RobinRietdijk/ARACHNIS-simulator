18e545662e7d838ec7d4b6aea6f0b4747993c385
Robin Rietdijk <robinrietdijk@kabelfoon.net>
2023-11-07 00:24:32 +0100

    Add base of circular slider component

---
+++ src/components/CircularRangeSlider/CircularRangeSlider.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="crs-container" :style="getContainerStyling">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+
+    }),
+    props: {
+        min: {
+            type: Number,
+            default: 0.
+        },
+        max: {
+            type: Number,
+            default: 100,
+        },
+        step: {
+            type: Number,
+            default: 1,
+        },
+        value: {
+            type: [Number, Array],
+            default: null,
+        },
+        radius: {
+            type: Number,
+            default: 100
+        },
+    },
+    methods: {
+        getContainerStyling() {
+            return {
+                width: this.radius * 2,
+                height: this.radius * 2
+            }
+        }
+    },
+    created: {
+        
+    }
+}
+</script>
+
+<style scoped>
+.crs-container {
+    border-radius: 50%;
+}
+</style>
